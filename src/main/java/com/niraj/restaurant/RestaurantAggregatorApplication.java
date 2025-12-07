7a94812acb8e0c0350e7a319b10eed1c9ba479a6
Niraj Adhikary <nirajmind@users.noreply.github.com>
2025-12-07 07:01:21 +0400

    Applied redis cache

--- src/main/java/com/niraj/restaurant/RestaurantAggregatorApplication.java
+++ src/main/java/com/niraj/restaurant/RestaurantAggregatorApplication.java
@@ -2,8 +2,10 @@ package com.niraj.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RestaurantAggregatorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/niraj/restaurant/service/RestaurantService.java
+++ src/main/java/com/niraj/restaurant/service/RestaurantService.java
@@ -6,6 +6,9 @@ import com.niraj.restaurant.dto.RestaurantSearchRequest;
 import com.niraj.restaurant.mapper.RestaurantMapper;
 import com.niraj.restaurant.repository.RestaurantRepository;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -15,11 +18,11 @@ import org.springframework.stereotype.Service;
 import java.util.Comparator;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class RestaurantService {
     private final RestaurantRepository repo;
     private final RestaurantMapper mapper = new RestaurantMapper();
-
-    public RestaurantService(RestaurantRepository repo) { this.repo = repo; }
 
     public Page<@NonNull RestaurantResponse> search(RestaurantSearchRequest req, Pageable pageable) {
         Specification<@NonNull Restaurant> spec = RestaurantSpecs.byRequest(req);
@@ -34,7 +37,11 @@ public class RestaurantService {
         return page.map(mapper::toDto);
     }
 
+    @Cacheable(value = "restaurants", key = "#id")
     public RestaurantResponse getById(Long id) {
+
+        log.info("Fetching Restaurant {} from Database...", id);
+
         Restaurant r = repo.findById(id).orElseThrow(() -> new RuntimeException("Restaurant not found"));
         return mapper.toDto(r);
     }

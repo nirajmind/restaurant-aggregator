8c8adce88c775fafd7bb3ef8c3aafab8cbea006a
Niraj Adhikary <nirajmind@users.noreply.github.com>
2025-12-07 08:02:27 +0400

    Corrected cache serialization

--- src/main/java/com/niraj/restaurant/dto/RestaurantResponse.java
+++ src/main/java/com/niraj/restaurant/dto/RestaurantResponse.java
@@ -1,10 +1,15 @@
 package com.niraj.restaurant.dto;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Set;
 
 public record RestaurantResponse(
         Long id, String name, String address, BigDecimal lat, BigDecimal lon,
         Short priceRange, BigDecimal avgRating, Set<String> cuisines, Set<String> tags
-) {}
+) implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
 
